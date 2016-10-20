935c445c440f35c17c7cdaa5a785be0d34e5e4dd
Ruchika Sood <sood.ruchika@siesgst.ac.in>
2016-10-20 23:50:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var ele =document.getElementById('min');
-//ele.innerHTML="new value" ; 
+ele.innerHTML="new value" ; 
 
 //Change the img poistion
 var img= document.getElementById('madi');
