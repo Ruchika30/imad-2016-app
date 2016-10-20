8735afc29dff1b0ee3a320c8fb6096116462134f
Ruchika Sood <sood.ruchika@siesgst.ac.in>
2016-10-20 23:47:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,16 @@ ele.innerHTML="new value" ;
 
 //Change the img poistion
 var img= document.getElementById('madi');
+
+function moveRight(){
+    marginLeft = marginLeft+10 ; 
+    img.style.marginLeft = marginLeft + ' px' ;
+}
+
 img.onclick = function(){
-    img.style.marginLeft = "100px";
-    
-    
-    
+    var interval = setInterval(moveRight,100)
 };
+
+var 
 
 
