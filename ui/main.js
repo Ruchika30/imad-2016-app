eeb17d6efc026cae165ae55929312f3e5037da76
Ruchika Sood <sood.ruchika@siesgst.ac.in>
2016-10-24 09:24:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 var ele =document.getElementById('min');
 ele.innerHTML="Hey i am new value" ; 
 
@@ -15,6 +15,24 @@ function moveLeft(){
 img.onclick = function(){
     
     var interval = setInterval(moveLeft,100);
+};*/
+ 
+ 
+var button = document.getElementById('counter');
+var counter =  0; 
+button.onclick = function(){
+    // make request to counter varibale
+    
+    //Capture the response in variable 
+    
+    //Render the variable in index
+    
+    counter  =counter +1;
+    var span =  document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+    
+    
 };
 
 
