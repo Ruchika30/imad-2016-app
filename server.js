ccec6690cbd50139a7d7302e6662ba85fff15e10
Ruchika Sood <sood.ruchika@siesgst.ac.in>
2016-11-10 08:52:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/articles/articleName', function (req, res) {
             res.status(500).send(err.toString());
         
     }else{
-        if{result.rows.length === 0){
+        if(result.rows.length === 0){
                 res.status(404).send("artcicle not found");
         }else{
             var articleData  = result.rows[0];
