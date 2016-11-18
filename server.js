baee32a3187baccbabd8352ab8e90101eefaca36
Ruchika Sood <sood.ruchika@siesgst.ac.in>
2016-11-18 17:37:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,17 +26,7 @@ var articles={
     },
 };
 
-function createtemplate (data){
-    var title = data.title;
-    var date= data.date;
-    var heading= data.heading;
-    var content= data.content;
-    
-    var htmlTemplate=
-        
-    
-    
-}
+
 
 
 
