c6222e0c143022b76d2ef2c4db748707a592ee4e
Ruchika Sood <sood.ruchika@siesgst.ac.in>
2016-11-23 11:11:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,8 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     //articlename is a var== article-one/ article-two etc
-  res.send(createtemplate())
+    var articleName = req.params.articleName;
+  res.send(createtemplate(articles(articleName)));
 });
 
 
