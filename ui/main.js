8bb6dea700675b3c6f43cffa76ec79a32b79b0bf
Ruchika Sood <sood.ruchika@siesgst.ac.in>
2016-11-24 08:29:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,7 +54,7 @@ img.onclick = function(){
      if(request.readyState === XMLHttpRequest.DONE)   {
          if(request.status === 200){//make request to render a list
               var names  = request.responseText;
-              names = JSON.parse(skills);               // to convert string to object
+              names = JSON.parse(names);               // to convert string to object
               var list='';
               
               for(var i=0;i < names.length; i++){
